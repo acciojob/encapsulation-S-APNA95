821990bdb07e6b3fc91149dc07de9c94fa812ee3
S-APNA95 <sapna.acharya3@gmail.com>
2024-02-05 19:13:55 +0530

    updated

--- src/main/java/com/driver/encapsulation/Main.java
+++ src/main/java/com/driver/encapsulation/Main.java
@@ -3,7 +3,8 @@ package com.driver.encapsulation;
 public class Main {
     public static void main(String[] args) {
         RWOnly rwOnly = new RWOnly();
-        rwOnly.setName("Welcome");
+        rwOnly.setName("Sapna");
         String name = rwOnly.getName();
+        System.out.println(name);
     }
 }
